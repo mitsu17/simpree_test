4cb8c6975980e034a79f95a58aacb9ae9b790e43
mitsu17 <66157250+mitsu17@users.noreply.github.com>
2020-08-09 10:22:01 +0900

    add_masonry_function_3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,42 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all
   end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+
+  # def create
+  #   @post = post.new(post_params)
+
+  #   respond_to do |format|
+  #     if @post.save
+  #       format.html { redirect_to @post, notice: 'Movie was successfully created.' }
+  #       # format.json { render :show, status: :created, location: @movie }
+  #     else
+  #       format.html { render :new }
+  #       # format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+
+  private
+  def post_params
+    params.permit(:video, :title, :text)
+  end
+
+
+
 
 end
