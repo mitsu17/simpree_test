b3d87aefa938a5775c91893d82c0e3f55650bda8
mitsu17 <66157250+mitsu17@users.noreply.github.com>
2020-08-11 11:31:56 +0900

    add_ masonry_function_4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create!(post_params)
+    @post = Post.create(post_params)
     redirect_to root_path
   end
 
